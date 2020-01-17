2c3e49a4eb84465b55890462a6d0f7b9ec4690ad
Farlely Felicia <f._felicia@outlook.com>
2020-01-17 20:25:41 +0100

    klaar

---
+++ Index.php
@@ -0,0 +1,11 @@
+
+<?php require 'header.php'; ?>
+
+
+
+
+<div class="container">
+  <h3>Right Aligned Navbar</h3>
+  <p>The .navbar-right class is used to right-align navigation bar buttons.</p>
+</div>
+<?php require 'footer.php'; ?>
